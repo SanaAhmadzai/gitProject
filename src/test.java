45b65843864a99c5031f15e6308aed348e0b87bb
SanaAhmadzai <105254552+SanaAhmadzai@users.noreply.github.com>
2022-05-11 12:48:55 -0400

    PULL OPTION

--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		
 	 System.out.println("This is message added to check the Git pull commend.");
+	 System.out.print ("This change is for Pull option");
 	}
 
 }
