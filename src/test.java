a5d355ea9766314e0687980b032fc6d652502469
SanaAhmadzai <105254552+SanaAhmadzai@users.noreply.github.com>
2022-05-11 13:18:42 -0400

    pull

--- src/test.java
+++ src/test.java
@@ -4,6 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		
 	 System.out.println("This is message added to check the Git pull commend.");
+		 System.out.println("pull request.");
+		
 	}
 
 }
