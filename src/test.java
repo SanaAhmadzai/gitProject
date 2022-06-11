410db4066ce226b7b3a4cce7179082c286ce4778
SanaAhmadzai <105254552+SanaAhmadzai@users.noreply.github.com>
2022-06-10 23:49:35 -0400

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		
 	 System.out.println("This is message added to check the Git pull commend.");
-	 System.out.print ("This change is for Pull option");
+	 System.out.print ("I broght these changes to check if the pull request works fine.");
 	}
 
 }
